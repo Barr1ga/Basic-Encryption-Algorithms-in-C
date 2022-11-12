2fcccb9a3913c13c6ea9cd8e175d17a8f9ec7c67
Hosea Barriga <midori.mizune@gmail.com>
2022-11-12 22:56:14 +0800

    encrypt decrypt

---
+++ test.c
@@ -0,0 +1,8 @@
+#include <math.h>
+#include <stdio.h>
+
+void main() {
+  double quo = (double)26 / 3;
+  printf("%lf\n", quo);
+  printf("%lf", ceil(quo));
+}
--- transpositional/transpositionalCipher_Barriga.c
+++ transpositional/transpositionalCipher_Barriga.c
@@ -1,23 +1,26 @@
 #include <conio.h>
 #include <ctype.h>
+#include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define MAX 100
+#define ENCRYPT 1
+#define DECRYPT 0
 
-char *transpositionalCipher(char plainText[], int key, char action[]);
-int charactersCount(char text[]);
+char *encrypt(char plainText[], char key[]);
+char *decrypt(char cipherText[], char key[]);
 void pause();
-bool requireKey(int key);
+bool requireKey(char key[]);
 void displayMatrix(char **matrix, int colCount, int rowCount);
+double charactersCount(char text[]);
 
 void main() {
   // Initial variable declaration
-  char plainText[MAX];
-  char *cipherText = NULL, *newPlainText = NULL;
-  int choice, key = 0;
+  char *cipherText = NULL, *newPlainText = NULL, key[MAX], plainText[MAX];
+  int choice;
 
   // Menu page
   do {
@@ -25,12 +28,12 @@ void main() {
     printf("---------------------\n");
     printf("[0] Exit\n");
     printf("[1] Set plain text\n");
-    printf("[2] Set key (row length)\n");
+    printf("[2] Set key\n");
     printf("[3] Encrypt plain text\n");
     printf("[4] Decrypt cipher text\n");
     printf("---------------------\n");
     printf("Plain text: %s\n", plainText);
-    printf("Key: %d\n", key);
+    printf("Key: %s\n", key);
     printf("Cipher text: %s\n", cipherText);
     printf("New plain text: %s\n", newPlainText);
     printf("---------------------\n");
@@ -46,9 +49,9 @@ void main() {
 
     // Request user key input
     if (choice == 2) {
-      printf("Set key (row length): ");
+      printf("Set key: ");
       fflush(stdin);
-      scanf("%d", &key);
+      scanf("%[^\n]", key);
     }
 
     // Run polyalphabetic encryption
@@ -59,7 +62,7 @@ void main() {
         if (cipherText != NULL) {
           free(cipherText);
         }
-        cipherText = transpositionalCipher(plainText, key, "encrypt");
+        cipherText = encrypt(plainText, key);
         printf("Cipher Text: %s\n", cipherText);
       }
     }
@@ -72,7 +75,7 @@ void main() {
         if (newPlainText != NULL) {
           free(newPlainText);
         }
-        newPlainText = transpositionalCipher(cipherText, key, "decrypt");
+        newPlainText = decrypt(cipherText, key);
         printf("Plain Text: %s\n", newPlainText);
       }
     }
@@ -109,8 +112,11 @@ void main() {
   *             n  = 2 + 1
   *             n  = 3
 */
-char *transpositionalCipher(char plainText[], int colCount, char action[]) {
-  int textIdx, rowIdx, colIdx, rowCount = charactersCount(plainText) / colCount;
+char *encrypt(char plainText[], char key[]) {
+  int textIdx, rowIdx, colIdx, colCount = strlen(key), rowCount;
+  rowCount = charactersCount(plainText) / colCount;
+
+  printf("%d", colCount);
   // Allocate pointer memory
   char *result = calloc(strlen(plainText), sizeof(char));
   char **matrix = calloc(rowCount, sizeof(char *));
@@ -119,22 +125,25 @@ char *transpositionalCipher(char plainText[], int colCount, char action[]) {
     printf("NULL");
     return NULL;
   }
-  printf("1");
 
   for (rowIdx = 0; rowIdx < rowCount; rowIdx++) {
     matrix[rowIdx] = calloc(colCount, sizeof(char));
   }
 
-  printf("2");
+  displayMatrix(matrix, colCount, rowCount);
 
-  for (textIdx = 0, colIdx = 0, rowIdx = 0; textIdx <= strlen(plainText);
+  printf("strlen = %d\n", strlen(plainText));
+  for (textIdx = 0, colIdx = 0, rowIdx = 0; textIdx < strlen(plainText);
        textIdx++) {
+    // printf("\n%d -> [%d][%d] = %c", textIdx, rowIdx, colIdx,
+    //        plainText[textIdx]);
+
     if (plainText[textIdx] == ' ') {
       textIdx++;
-    } else {
-      matrix[rowIdx][colIdx] = plainText[textIdx];
     }
-    printf("test");
+
+    matrix[rowIdx][colIdx] = plainText[textIdx];
+
     if (colIdx == colCount - 1) {
       rowIdx++;
       colIdx = 0;
@@ -144,15 +153,13 @@ char *transpositionalCipher(char plainText[], int colCount, char action[]) {
   }
 
   displayMatrix(matrix, colCount, rowCount);
-  printf("3");
 
-  for (colIdx = 0, rowCount = 0, textIdx = 0; textIdx <= strlen(plainText);
+  for (colIdx = 0, rowIdx = 0, textIdx = 0; textIdx < strlen(plainText);
        textIdx++) {
     if (plainText[textIdx] == ' ') {
-      result[textIdx] = plainText[textIdx];
+      result[textIdx] = ' ';
     } else {
-      result[textIdx] = matrix[colIdx][rowIdx];
-      colIdx++;
+      result[textIdx] = matrix[rowIdx][colIdx];
 
       if (rowIdx == rowCount - 1) {
         rowIdx = 0;
@@ -161,19 +168,68 @@ char *transpositionalCipher(char plainText[], int colCount, char action[]) {
         rowIdx++;
       }
     }
-
-    return result;
   }
+
+  return result;
 }
 
-int charactersCount(char text[]) {
-  int count = 0;
-  for (int textIdx = 0; textIdx <= strlen(text); textIdx++) {
-    if (text[textIdx] != ' ') {
-      count++;
+char *decrypt(char cipherText[], char key[]) {
+  int textIdx, rowIdx, colIdx, colCount = strlen(key), rowCount;
+  rowCount = charactersCount(cipherText) / colCount;
+  // Allocate pointer memory
+  char *result = calloc(strlen(cipherText), sizeof(char));
+  char **matrix = calloc(rowCount, sizeof(char *));
+  // guard clause to fail cipher if memory wasnt allocated
+  if (matrix == NULL || result == NULL) {
+    printf("NULL");
+    return NULL;
+  }
+
+  for (rowIdx = 0; rowIdx < rowCount; rowIdx++) {
+    matrix[rowIdx] = calloc(colCount, sizeof(char));
+  }
+
+  displayMatrix(matrix, colCount, rowCount);
+
+  printf("strlen = %d\n", strlen(cipherText));
+  for (textIdx = 0, colIdx = 0, rowIdx = 0; textIdx < strlen(cipherText);
+       textIdx++) {
+    // printf("\n%d -> [%d][%d] = %c", textIdx, rowIdx, colIdx,
+    //        cipherText[textIdx]);
+
+    if (cipherText[textIdx] == ' ') {
+      textIdx++;
+    }
+
+    matrix[colIdx][rowIdx] = cipherText[textIdx];
+
+    if (rowIdx == rowCount - 1) {
+      colIdx++;
+      rowIdx = 0;
+    } else {
+      rowIdx++;
     }
   }
-  return count;
+
+  displayMatrix(matrix, colCount, rowCount);
+
+  for (colIdx = 0, rowIdx = 0, textIdx = 0; textIdx < strlen(cipherText);
+       textIdx++) {
+    if (cipherText[textIdx] == ' ') {
+      result[textIdx] = ' ';
+    } else {
+      result[textIdx] = matrix[colIdx][rowIdx];
+
+      if (colIdx == colCount - 1) {
+        colIdx = 0;
+        rowIdx++;
+      } else {
+        colIdx++;
+      }
+    }
+  }
+
+  return result;
 }
 
 void pause() {
@@ -184,8 +240,8 @@ void pause() {
 /*
   Function to require key input from user
 */
-bool requireKey(int key) {
-  if (key <= 0) {
+bool requireKey(char *key) {
+  if (key == NULL) {
     printf("Please enter a valid key!\n");
     return false;
   }
@@ -193,12 +249,24 @@ bool requireKey(int key) {
   return true;
 }
 
+double charactersCount(char text[]) {
+  double count = 0;
+  for (int textIdx = 0; textIdx <= strlen(text); textIdx++) {
+    if (text[textIdx] != ' ') {
+      count++;
+    }
+  }
+
+  return count;
+}
+
 /*
     Function to display the current values in the matrix. This function is for
    debug only
 */
 void displayMatrix(char **matrix, int colCount, int rowCount) {
-  printf("matrix: \n");
+  printf("\n");
+
   for (int rowIdx = 0; rowIdx < rowCount; rowIdx++) {
     for (int colIdx = 0; colIdx < colCount; colIdx++) {
       printf("[%c] ", matrix[rowIdx][colIdx]);
