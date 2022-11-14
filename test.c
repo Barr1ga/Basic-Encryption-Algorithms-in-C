5b947de9e2371f912461acebd993b12f0022636e
Barriga <hor.barr1ga@gmail.com>
2022-11-14 20:14:59 +0800

    commit

--- test.c
+++ test.c
@@ -1,7 +1,18 @@
+<<<<<<< Updated upstream
 #include <math.h>
+=======
+>>>>>>> Stashed changes
 #include <stdio.h>
 #include <stdlib.h>
 
 void main() {
+<<<<<<< Updated upstream
   printf("%d", 3 ^ 4);
+=======
+  char arr[10] = "hello";
+  char *str = calloc(10, sizeof(char));
+  scanf("%s", str);
+  printf("%s", arr);
+  printf("%s", str);
+>>>>>>> Stashed changes
 }
